76581ea23211800f26a96c0cc3b2368a54de5b42
Valentin OVD (12) <x@x.fr>
2015-04-30 10:21:50 -0400

    Update the script

--- botscript.sh
+++ botscript.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+echo "Type a word and enter :)"
+echo "For save your work and create the bot.pot, type break and enter"
 while true
-do read -p "Word ? (break for save)\n" text && echo $text >> original
+do read -p "Word ? " text
 if [ $text = "break" ]
 then break
 fi
+echo $text >> original
 done
 for i in `cat original | sed 's/\n//'` ; do echo "msgid \"$i\"\\nmsgstr \"\"\\n" >> bot.pot ; done
+rm original
