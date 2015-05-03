a8e8698d1a0569762b2d2f795f30e4bd3bea4d5a
vovd <vovd.tanglu@gmail.com>
2015-05-03 17:24:07 +0200

    Update sweetbuild.sh

--- sweetbuild.sh
+++ sweetbuild.sh
@@ -2,6 +2,6 @@
 
 mkdir build && cd build
 msgfmt ../en-fr.po -o en-fr.mo
-
+msgfmt ../en-fr_rev.po -o en-fr_rev.mo
 echo "You can now use gettext for translate word !"
 echo -e 'Example :\necho "\`gettext en-fr computer\`'
