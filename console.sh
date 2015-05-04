370c017fe93b096703df7bf74ed93df83ad365d6
Valentin OVD (12) <x@x.fr>
2015-05-04 15:03:34 -0400

    console.sh is now standalone

--- console.sh
+++ console.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
-
+export PO=0
 clear
 echo -e "Welcome on the Open Word Database Console !\n"
 wget https://github.com/ValentinOVD/Open-Word-Database/raw/master/botscript.sh -O botscript.sh
-chmod +x botscript.sh
+wget https://github.com/ValentinOVD/Open-Word-Database/raw/master/translation-en-to-x.sh -O translation-en-to-x.sh
+wget https://github.com/ValentinOVD/Open-Word-Database/raw/master/translation-x-to-en.sh -O translation-x-to-en.sh
+chmod +x botscript.sh translation-x-to-en.sh translation-en-to-x.sh
 while true
 do read -p ">" text
 if [ $text = "break" ]
 then break
 fi
+if [ $text = "translate" ]
+then if [ $PO = "0" ]
+then echo "For this function, you need to have installed the po files. By using the po command"
+fi
+if [ $PO = "1" ]
+then read -p "What language ?" textd
+read -p "en-to-$textd or $textd-to-en ? (Answer 1 or 2)" text
+read -p "What do you want to translate ?" texta
+
+if [ $text = "1" ]
+then ./translation-en-to-x.sh $textd $texta
+fi
+if [ $text = "2" ]
+then ./translation-x-to-en.sh $textd $texta
+fi
+fi
+fi
 if [ $text = "help" ]
-then echo -e "break\nbot\npo\nmo\nyes\nno"
+then echo -e "break\nbot\npo\nmo\ntranslate\yes\nno"
 fi
 if [ $text = "transifex" ]
 then echo "https://www.transifex.com/projects/p/database/"
@@ -28,6 +47,9 @@ tar -xvf botupload.tar.gz
 mkdir po
 mv Open-Word-Database-botupload/*.po po/
 rm -r Open-Word-Database-botupload botupload.tar.gz
+mv po/* ./
+export PO=1
+echo "Done without any problem"
 fi
 fi
 
@@ -50,5 +72,5 @@ fi
 fi
 fi
 
-read
 done
+rm -r po mo
