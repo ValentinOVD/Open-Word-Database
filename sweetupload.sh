45903b853fbc7f00fbb2116e421d6664ffd654a7
vovd <vovd.tanglu@gmail.com>
2015-05-06 12:02:50 +0200

    Update sweetupload.sh

--- sweetupload.sh
+++ sweetupload.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false3" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=binary-master https://${GH_TOKEN}@github.com/ValentinOVD/Open-Word-Database binary > /dev/null
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/ValentinOVD/Open-Word-Database-bin binary > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd binary
@@ -18,8 +18,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false3" ]; then
 
   #add, commit and push files
   git add -A .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to binary-master"
-  git push -fq origin binary-master > /dev/null
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to binary"
+  git push -fq origin master > /dev/null
 
   echo -e "Done magic\n"
 fi
