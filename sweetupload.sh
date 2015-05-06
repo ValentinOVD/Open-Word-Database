bef573d5b3c24ec78c27dc207f08f74c0684df66
vovd <vovd.tanglu@gmail.com>
2015-05-06 12:07:05 +0200

    Update sweetupload.sh

--- sweetupload.sh
+++ sweetupload.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_PULL_REQUEST" == "false3" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update binary-master\n"
 
   #copy data we're interested in to other place
