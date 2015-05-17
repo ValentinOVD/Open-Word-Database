19c8a1712436f663e41b8cc719305db34103e49c
vovd <vovd.tanglu@gmail.com>
2015-05-17 17:13:49 +0200

    Delete sweetinstall.sh

--- sweetinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-msgfmt en-fr.po -o en-fr.mo
-sudo mv en-fr.mo /usr/share/locale/fr/LC_MESSAGES/
-
-echo "You can now use gettext for translate word !"
-echo -e 'Example :\necho "\`gettext en-fr computer\`'
